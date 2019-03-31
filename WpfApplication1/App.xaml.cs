01d1c9446ae55907222e080100aaaa40bae8e7f2
Anna Mikhailova <AMikhailova3@veeam.com>
2019-03-31 17:34:22 +0300

    add plot

--- WpfApplication1/App.xaml.cs
+++ WpfApplication1/App.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace WpfApplication1
 {
@@ -13,5 +16,19 @@ namespace WpfApplication1
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var culture = new CultureInfo("ru-RU");
+
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+              typeof(FrameworkElement),
+              new FrameworkPropertyMetadata(
+                  XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+
+            base.OnStartup(e);
+        }
     }
 }
--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Win32;
+using OxyPlot;
+using OxyPlot.Annotations;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 
 namespace WpfApplication1
 {
@@ -21,11 +14,7 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            cb.SelectedIndex = 0;
-        }
+        public PlotModel MyModel { get; private set; }
 
         private void btnOpenFiles_Click(object sender, RoutedEventArgs e)
         {
@@ -39,5 +28,77 @@ namespace WpfApplication1
                    // lbFiles.Items.Add(Path.GetFileName(filename));
             }
         }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            cb.SelectedIndex = 0;
+
+
+            MyModel = new PlotModel { Title = "Your Equation", LegendTitle = "Equations" };
+            this.DataContext = this;
+
+            this.plot.Model = this.CreateModel(new DateTime(2010, 01, 01), new DateTime(2011, 01, 01), 1);
+
+            int year = DateTime.Now.Year;
+
+        }
+
+        public Collection<DateValue> Data { get; set; }
+
+
+        private PlotModel CreateModel(DateTime start, DateTime end, int increment)
+        {
+            var tmp = new PlotModel { Title = "DateTime axis (PlotModel)" };
+            tmp.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Minimum = -Math.PI / 2, Maximum = 3 * Math.PI / 2, TextColor = OxyColors.White, TicklineColor = OxyColors.White});
+            var r = new Random(13);
+            var date = start;
+            double i = -Math.PI / 2;
+            string[] months = { "янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен", "окт", "ноя", "дек", "окт", "ноя", "дек" };
+            int y = 0;
+            while (i < 3 * Math.PI / 2 )
+            {
+                LineAnnotation Line = new LineAnnotation
+                {
+                    
+                    Color = OxyColors.Black,
+                    Type = LineAnnotationType.Vertical,
+
+                    MaximumY = 1,
+                    Text = months[y],
+                    TextColor = OxyColors.Black,
+                    X = i,
+                    ToolTip = i.ToString(),
+                    TextOrientation = AnnotationTextOrientation.Horizontal,
+                     TextVerticalAlignment = OxyPlot.VerticalAlignment.Bottom,
+                     TextHorizontalAlignment = OxyPlot.HorizontalAlignment.Left,
+                };
+
+                tmp.Annotations.Add(Line);
+
+                i += Math.PI / 6;
+                y++;
+            }
+
+
+            var categoryAxis = new CategoryAxis()
+            {
+                Position = AxisPosition.Bottom
+           };
+            tmp.Axes.Add(categoryAxis);
+
+            tmp.Series.Add(new FunctionSeries(x => Math.Sin(2*x), -Math.PI / 2, 3 * Math.PI / 2, 0.1, "sin(x)"));
+
+            return tmp;
+
+        }
     }
+
+    public class DateValue
+    {
+        public DateTime Date { get; set; }
+        public double Value { get; set; }
+    }
+
 }
