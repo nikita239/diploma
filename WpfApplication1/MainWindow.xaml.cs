7b34faee575534f668cf698d7d49d1476b1a5e98
Anna Mikhailova <AMikhailova3@veeam.com>
2019-04-13 16:08:09 +0300

    excel reading

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -1,67 +1,116 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Windows;
+using Microsoft.Office.Interop.Excel;
 using Microsoft.Win32;
 using OxyPlot;
 using OxyPlot.Annotations;
 using OxyPlot.Axes;
 using OxyPlot.Series;
+using WpfApplication1.Properties;
+using Excel = Microsoft.Office.Interop.Excel;
+using Window = System.Windows.Window;
 
 namespace WpfApplication1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public PlotModel MyModel { get; private set; }
-
+        public string FileName { get; set; }
         private void btnOpenFiles_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Multiselect = true;
-            openFileDialog.Filter = "Excel files (*.xls)|(*.xlsx)|All files (*.*)|*.*";
-            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog.Multiselect = false;
+            openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            openFileDialog.RestoreDirectory = true;
+
+
             if (openFileDialog.ShowDialog() == true)
             {
-                //foreach (string filename in openFileDialog.FileNames)
-                   // lbFiles.Items.Add(Path.GetFileName(filename));
+                TableList.Clear();
+                Excel.Application xlApp = new Excel.Application();
+                Workbook xlWorkbook = xlApp.Workbooks.Open(openFileDialog.FileName);
+                _Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+                Range xlRange = xlWorksheet.UsedRange;
+                fileNameLabel.Content = openFileDialog.FileName;
+                int rowCount = xlRange.Rows.Count;
+                int colCount = xlRange.Columns.Count;
+
+                DataObject model = new DataObject();
+                Range cell;
+
+                for (int i = 1; i <= rowCount; i++)
+                {
+                    for (int j = 1; j <= colCount; j++)
+                    {
+                        cell = (Range)xlRange.Cells[i, j];
+                        //new line
+                        if (j == 1)
+                        {
+                            if (xlRange.Cells[i, j] != null)
+                            {
+                                if (cell.Value != null)
+                                {
+                                    if (i != 1)
+                                    {
+                                        TableList.Add(model);
+                                        model = new DataObject();
+                                    }
+                                    model.Letter = Convert.ToString(cell.Value);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            cell = (Range)xlRange.Cells[i, j];
+                            model.Sum += Convert.ToInt32(cell.Value);
+                        }
+                    }
+                    if (i == rowCount)
+                    {
+                        TableList.Add(model);
+                    }
+                }
+
+                Settings.Default.Save();
             }
         }
+        public class DataObject
+        {
+            public string Letter { get; set; }
+            public int Sum { get; set; }
+        }
 
+
+        private ObservableCollection<DataObject> TableList = new ObservableCollection<DataObject>();
 
         public MainWindow()
         {
             InitializeComponent();
             cb.SelectedIndex = 0;
 
-
             MyModel = new PlotModel { Title = "Your Equation", LegendTitle = "Equations" };
+
             this.DataContext = this;
 
-            this.plot.Model = this.CreateModel(new DateTime(2010, 01, 01), new DateTime(2011, 01, 01), 1);
+            this.plot.Model = this.CreateModel();
 
-            int year = DateTime.Now.Year;
+            this.dataGrid1.ItemsSource = TableList;
 
         }
 
-        public Collection<DateValue> Data { get; set; }
-
-
-        private PlotModel CreateModel(DateTime start, DateTime end, int increment)
+        private PlotModel CreateModel()
         {
             var tmp = new PlotModel { Title = "DateTime axis (PlotModel)" };
-            tmp.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Minimum = -Math.PI / 2, Maximum = 3 * Math.PI / 2, TextColor = OxyColors.White, TicklineColor = OxyColors.White});
-            var r = new Random(13);
-            var date = start;
+            tmp.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Minimum = -Math.PI / 2, Maximum = 3 * Math.PI / 2, TextColor = OxyColors.White, TicklineColor = OxyColors.White });
             double i = -Math.PI / 2;
             string[] months = { "янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен", "окт", "ноя", "дек", "окт", "ноя", "дек" };
             int y = 0;
-            while (i < 3 * Math.PI / 2 )
+            while (i < 3 * Math.PI / 2)
             {
                 LineAnnotation Line = new LineAnnotation
                 {
-                    
+
                     Color = OxyColors.Black,
                     Type = LineAnnotationType.Vertical,
 
@@ -71,8 +120,8 @@ namespace WpfApplication1
                     X = i,
                     ToolTip = i.ToString(),
                     TextOrientation = AnnotationTextOrientation.Horizontal,
-                     TextVerticalAlignment = OxyPlot.VerticalAlignment.Bottom,
-                     TextHorizontalAlignment = OxyPlot.HorizontalAlignment.Left,
+                    TextVerticalAlignment = OxyPlot.VerticalAlignment.Bottom,
+                    TextHorizontalAlignment = OxyPlot.HorizontalAlignment.Left,
                 };
 
                 tmp.Annotations.Add(Line);
@@ -85,20 +134,14 @@ namespace WpfApplication1
             var categoryAxis = new CategoryAxis()
             {
                 Position = AxisPosition.Bottom
-           };
+            };
+
             tmp.Axes.Add(categoryAxis);
 
-            tmp.Series.Add(new FunctionSeries(x => Math.Sin(2*x), -Math.PI / 2, 3 * Math.PI / 2, 0.1, "sin(x)"));
+            tmp.Series.Add(new FunctionSeries(x => Math.Sin(2 * x), -Math.PI / 2, 3 * Math.PI / 2, 0.1, "sin(x)"));
 
             return tmp;
 
         }
     }
-
-    public class DateValue
-    {
-        public DateTime Date { get; set; }
-        public double Value { get; set; }
-    }
-
 }
