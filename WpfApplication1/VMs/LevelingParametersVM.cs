b08673ca57d6a05de97a2f60e388d003144ec196
Anna Mikhailova <AMikhailova3@veeam.com>
2019-05-30 20:34:12 +0300

    display

--- WpfApplication1/VMs/LevelingParametersVM.cs
+++ WpfApplication1/VMs/LevelingParametersVM.cs
@@ -1,25 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Data;
+using System.Windows.Input;
 
 namespace WpfApplication1
 {
-    public class LevelingParametersVM: BaseViewModel
+    public class DataStructure
+    {
+        public string TextBlock { get; set; }
+        public string TextBlock2 { get; set; }
+        public double TextBox { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class LevelingParametersVM : BaseViewModel
     {
         private List<string> _parameters;
-        private string       _selectedParameter;
-        private bool         _visibilityCondition;
-
+        private string _selectedParameter;
+        private bool _visibilityCondition;
+        private List<DataStructure> _cells;
+        private List<DataStructure> _allValues;
+        private string _displayString;
 
         public LevelingParametersVM()
         {
             Parameters = new List<string> { "L", "T", "M", "V" };
-            //VisibilityCondition = _selectedParameter
+            _allValues = new List<DataStructure>() {
+                new DataStructure{
+                    TextBlock = "L",
+                    Description = "Протяженность линии нивелирования"
+                },
+                new DataStructure{
+                    TextBlock = "T",
+                    Description = "Периодичность нивелирования"
+                },
+                new DataStructure{
+                    TextBlock = "M",
+                    Description = "Средняя квадратичная ошибка нивелирования на 1 км хода"
+                },
+                new DataStructure{
+                    TextBlock = "V",
+                    TextBlock2 = "прогн",
+                    Description = "Прогнозная скорость"
+                }
+            };
+            Cells = new List<DataStructure>();
         }
 
         public List<string> Parameters
@@ -40,53 +65,81 @@ namespace WpfApplication1
             {
                 if (_selectedParameter == value) return;
                 _selectedParameter = value;
+                var item = _allValues.Where(x => x.TextBlock == value);
+               // if(item !)
+                Cells = _allValues.Except(item).ToList();
                 OnPropertyChanged("SelectedParameter");
             }
         }
 
-        public bool VisibilityCondition
+        public List<DataStructure> Cells
         {
-            get { return _visibilityCondition; }
+            get { return _cells; }
             set
             {
-                if (_visibilityCondition != value)
+                if (_cells != value)
                 {
-                    _visibilityCondition = value;
-                    OnPropertyChanged("VisibilityCondition");
+                    _cells = value;
+                    OnPropertyChanged("Cells");
                 }
             }
         }
 
-      
-    }
-
-    public class IsVisibleToGridLengthConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo language)
+        public string DisplayString
         {
-            try
+            get { return _displayString; }
+            set
             {
-                GridUnitType t = GridUnitType.Star;
-                if (parameter != null)
+                if (_displayString != value)
                 {
-                    Enum.TryParse<GridUnitType>((string)parameter, true, out t);
+                    _displayString = value;
+                    OnPropertyChanged("DisplayString");
                 }
-
-                if (value != null)
-                {
-                    bool d = (bool)value;
-                    return d == false ? new GridLength(0, GridUnitType.Star) : new GridLength(1, t);
-                }
-                return null;
-            }
-            catch (Exception exp)
-            {
-                return null;
             }
         }
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo language)
+
+
+        public ICommand ButtonClickCommand
         {
-            return null;
+            get { return new DelegateCommand(FormatDisplayString, true); }
+        }
+
+
+
+        private void FormatDisplayString()
+        {
+            var l = _cells.FirstOrDefault(x => x.TextBlock == "L")?.TextBox;
+            var m = _cells.FirstOrDefault(x => x.TextBlock == "M")?.TextBox;
+            var v = _cells.FirstOrDefault(x => x.TextBlock == "V")?.TextBox;
+            var t = _cells.FirstOrDefault(x => x.TextBlock == "T")?.TextBox;
+            double? value = 0;
+            switch (_selectedParameter)
+            {
+                case "L":
+                    {
+                        value = t * t * v * v / (32 * m * m);
+                        DisplayString = String.Concat("L ≤ ", Math.Round(value.Value, 1));
+                        break;
+                    }
+                case "M":
+                    {
+                        value = t * (v / Math.Sqrt(32 * l.Value));
+                        DisplayString = String.Concat("M ≤ ", Math.Round(value.Value, 1));
+                        break;
+                    }
+                case "V":
+                    {
+                        value = m * Math.Sqrt(32 * l.Value) / t;
+                        DisplayString = String.Concat("V ≥ ", Math.Round(value.Value, 1));
+                        break;
+                    }
+                case "T":
+                    {
+                        value = m * Math.Sqrt(32 * l.Value) / v;
+                        DisplayString = String.Concat("T ≥ ", Math.Round(value.Value, 1 ));
+                        break;
+                    }
+            }
         }
     }
 }
